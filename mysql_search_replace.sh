d608826a73765a5a49c1abf85767ad7d85349e28
Jerry Steele <ticktockhouse@gmail.com>
2015-08-19 18:14:05 +0100

    A handy mysql search/replace script

---
+++ mysql_search_replace.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo -n "Enter database name: " ; read db_name
+echo -n "Enter search string: " ; read search_string
+echo -n "Enter replacement string: " ; read replacement_string
+ 
+MYSQL="/usr/bin/mysql --skip-column-names" 
+ 
+echo "SHOW TABLES;" | $MYSQL $db_name | while read db_table
+do
+	echo "SHOW COLUMNS FROM $db_table;" | $MYSQL $db_name| 
+	awk -F't' '{print $1}' | while read tbl_column
+	do
+		echo "update $db_table set ${tbl_column} = replace(${tbl_column}, '${search_string}', '${replacement_string}');" 
+#|
+#		$MYSQL $db_name
+	done
+done
