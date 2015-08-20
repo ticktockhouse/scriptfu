df8753779c0b9fc19266fe3fcb367d333951b75d
Jerry Steele <ticktockhouse@gmail.com>
2015-08-20 09:09:09 +0100

    Small correction, otherwise awk IFS is 't', not '\t'

--- mysql_search_replace.sh
+++ mysql_search_replace.sh
@@ -8,7 +8,7 @@ MYSQL="/usr/bin/mysql --skip-column-names"
 echo "SHOW TABLES;" | $MYSQL $db_name | while read db_table
 do
 	echo "SHOW COLUMNS FROM $db_table;" | $MYSQL $db_name| 
-	awk -F't' '{print $1}' | while read tbl_column
+	awk -F '\t' '{print $1}' | while read tbl_column
 	do
 		echo "update $db_table set ${tbl_column} = replace(${tbl_column}, '${search_string}', '${replacement_string}');" |
 		$MYSQL $db_name
